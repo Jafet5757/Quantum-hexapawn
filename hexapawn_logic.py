eaa0e8cbd1dfc67ffc66b67b4efc25c10cc3b171
Kevin Jafet Moran Orozco <61211139+Jafet5757@users.noreply.github.com>
2024-12-11 20:00:33 -0600

    play with random movements

--- hexapawn_logic.py
+++ hexapawn_logic.py
@@ -1,5 +1,7 @@
 from prettytable import PrettyTable
 import pandas as pd
+import random
+import time
 
 class Hexapawn:
   def __init__(self):
@@ -7,6 +9,9 @@ class Hexapawn:
     self.current_player = 'O'
 
   def print_board(self):
+    """ 
+    Print the current board 
+    """
     print("\033c", end="") # Clear the console
     
     for i, row in enumerate(self.board):
@@ -18,6 +23,12 @@ class Hexapawn:
                   '-', '-', '-']
 
   def create_play(self):
+    """ 
+    Create a specifica play for the current player
+
+    -------
+    return: None 
+    """
     self.clean_board()
     print("Ingrese la jugada a realizar")
     print("Formato: x1, x2, x3, o1, o2, o3")
@@ -34,6 +45,17 @@ class Hexapawn:
     self.save_play()
 
   def get_representations(self, board=None):
+    """ 
+    Get the general, x player and o player representations of the board in binary format
+
+    Parameters:
+    board: list
+      The board to get the representations
+
+    -------
+    return: tuple
+      The general, x player and o player representations of the board
+    """
     board = self.board if board is None else board
     general = ''.join(map((lambda x: '1' if x == 'X' or x =='O' else '0'), board))
     x_player = ''.join(map((lambda x: '1' if x == 'X' else '0'), board))
@@ -76,6 +98,12 @@ class Hexapawn:
       
 
   def get_moves(self):
+    """ 
+    Returns a list of possible moves for the current player
+
+    -------
+    return: list of tuples with the possible moves 
+    """
     moves = []
     for i, p in enumerate(self.board):
       if p == 'X':
@@ -99,11 +127,121 @@ class Hexapawn:
           moves.append((g, x, o))
 
     return moves
+  
+  def is_valid_move(self, current_index:int, new_index:int, current_player:str = 'O')->bool:
+    """ 
+    Check if the move is valid 
+
+    Parameters:
+    current_index: int
+      The index of the current piece
+    new_index: int
+      The index of the new position
+    current_player: str
+      The current player
+
+    -------
+    return: bool
+      True if the move is valid, False otherwise
+    """
+    current_index = int(current_index) - 1
+    new_index = int(new_index) - 1
+    enemy = 'X' if current_player == 'O' else 'O'
+    # Go to up
+    if current_player == "O":
+      if new_index == current_index - 3 and current_index - 3 >= 0:
+        return True
+      if new_index == current_index - 4 and current_index % 3 != 0 and self.board[current_index-4] == enemy and current_index - 4 >= 0:
+        return True
+      if new_index == current_index - 2 and current_index % 3 != 2 and self.board[current_index-2] == enemy and current_index - 2 >= 0:
+        return True
+    # Go to down
+    else:
+      if new_index == current_index + 3 and current_index + 3 < 9:
+        return True
+      if new_index == current_index + 4 and current_index % 3 != 0 and self.board[current_index+4] == enemy and current_index + 4 < 9:
+        return True
+      if new_index == current_index + 2 and current_index % 3 != 2 and self.board[current_index+2] == enemy and current_index + 2 < 9:
+        return True
+    return False
+
+  def user_move(self):
+    """ 
+    Ask the user for a move and update the board if the move is valid 
+    """
+    while True:
+      try:
+        move = input("Ingresa la casilla actual y la nueva [1 2]: ")
+        move = move.split(" ")
+        move = list(map(int, move))
+        isValid = self.is_valid_move(move[0], move[1])
+        if isValid:
+          # Update board
+          self.board[move[1]-1] = self.board[move[0]-1]
+          self.board[move[0]-1] = '-'
+          self.print_board()
+          break
+        else:
+          print("Movimiento inválido")
+          continue
+      except:
+        c = input("\nError, Desea parar? (s/n): ")
+        if c == 's':
+          break
+        else:
+          continue
+
+  def get_random_move(self):
+    """ 
+     Get a random move for the machine player
+    """
+    moves = self.get_moves()
+    selected = random.choice(moves) # Tuple: (general, x, o)
+    # Update board
+    for i, p in enumerate(selected[1]):
+      # Update x player
+      if selected[1][i] == '1':
+        self.board[i] = 'X'
+      # Update o player
+      if selected[2][i] == '1':
+        self.board[i] = 'O'
+      # Update empty space
+      if selected[0][i] == '0':
+        self.board[i] = '-'
+    self.print_board()
+  
+  def game_finished(self)->bool:
+    """ 
+    Check if the game is finished 
+
+    -------
+    return: bool
+      True if the game is finished, False otherwise
+    """
+    if 'X' not in self.board:
+      print("Ganó el jugador O")
+      return True
+    if 'O' not in self.board:
+      print("Ganó el jugador X")
+      return True
+    representations = self.get_representations()
+    # id there are 1 in the first 3 positions of the x player representation, the player X won
+    if '1' in representations[1][6:]:
+      print("Ganó el jugador X")
+      return True
+    # id there are 1 in the last 3 positions of the x player representation, the player O won
+    if '1' in representations[2][:3]:
+      print("Ganó el jugador O")
+      return True
+    return False
 
 if __name__ == '__main__':
+
+  game = Hexapawn()
+  game.print_board()
   while True:
-    game = Hexapawn()
-    game.print_board()
-    game.create_play()
-    if input("\nDesea parar? (s/n): ") == 's':
+    game.user_move()
+    time.sleep(1)
+    game.get_random_move()
+    if game.game_finished():
       break
